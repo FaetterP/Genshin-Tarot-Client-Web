218fbb80abe4ffe09230deb57f03f8cc431fb654
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-04-21 10:31:48 +0300

    Добавлены стили для карт
    • Добавлены статы для карт

--- src/Components/Game/Card/Card.tsx
+++ src/Components/Game/Card/Card.tsx
@@ -1,9 +1,27 @@
 import { CardPrimitive } from "../../../../types/general";
+import { cards } from "../../../storage/Cards/cards";
+import { rusCards } from "../../../storage/Cards/names";
+import styles from "./Card.module.scss";
 
-export default function Card(props:CardPrimitive){
-    return(
-        <div>
-            {props.name}
-        </div>
-    )
+export default function Card(props: CardPrimitive) {
+  const { cost, isUpgraded } = cards[props.name];
+  const name = rusCards[props.name];
+  const description =
+    "description description description description description";
+
+  let costText = "";
+  for (let i = 0; i < 3 - cost; i++) {
+    costText += "◻";
+  }
+  for (let i = 0; i < cost; i++) {
+    costText += "⬜";
+  }
+
+  return (
+    <div className={styles.cardBlock}>
+      <div className={styles.cost}>{costText}</div>
+      <div className={styles[isUpgraded ? "upgradedName" : "name"]}>{name}</div>
+      <div className={styles.description}>{description}</div>
+    </div>
+  );
 }
